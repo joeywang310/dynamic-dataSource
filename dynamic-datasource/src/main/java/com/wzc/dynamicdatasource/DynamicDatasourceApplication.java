f430ebaf673001ff4382e11d213ca4a072748861
wangzhicheng <862752581@qq.com>
2019-01-08 19:12:53 +0800

    init

---
+++ dynamic-datasource/src/main/java/com/wzc/dynamicdatasource/DynamicDatasourceApplication.java
@@ -0,0 +1,14 @@
+package com.wzc.dynamicdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicDatasourceApplication.class, args);
+    }
+
+}
+
